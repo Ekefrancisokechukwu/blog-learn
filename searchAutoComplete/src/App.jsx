737a08f4e911420f7e129183e849c3fc855e844f
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2023-10-20 15:19:43 +0100

    added footer

--- searchAutoComplete/src/App.jsx
+++ searchAutoComplete/src/App.jsx
@@ -1,4 +1,6 @@
+import logo from "../src/assets/logo.svg";
 import Search from "./components/search/Search";
+import { AiFillGithub } from "react-icons/ai";
 
 function App() {
   return (
@@ -10,6 +12,21 @@ function App() {
       <div className="mt-10">
         <Search />
       </div>
+
+      <footer className="flex items-center gap-5">
+        <div className="flex gap-2 items-center">
+          <img src={logo} alt="logo" className="w-[1.5rem]" />{" "}
+          <h2 className="text-gray-500 text-xl font-bold">specter</h2>
+        </div>
+        <a
+          href="https://github.com/Ekefrancisokechukwu/blog-learn/tree/main/searchAutoComplete"
+          target="_blank"
+          rel="noreferrer"
+          className="text-xl"
+        >
+          <AiFillGithub />
+        </a>
+      </footer>
     </main>
   );
 }
