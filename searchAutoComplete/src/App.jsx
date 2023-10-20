6da2cd3ddd8a91e08385a275c5ecf5c91b0841ca
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2023-10-20 11:13:09 +0100

    titel update

--- searchAutoComplete/src/App.jsx
+++ searchAutoComplete/src/App.jsx
@@ -3,7 +3,9 @@ import Search from "./components/search/Search";
 function App() {
   return (
     <main className="bg-gray-200 h-screen pt-4 px-6">
-      <h1 className="text-center text-2xl mt-2">Search Autocomplete</h1>
+      <h1 className="text-center text-2xl mt-2">
+        Search Autocomplete Searchbar
+      </h1>
 
       <div className="mt-10">
         <Search />
